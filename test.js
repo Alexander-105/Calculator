6595c491c3f9927a208a7ba61f5fb71e00a6c9b5
Alexander Vynohradov <w_alexandr_i@ukr.net>
2022-12-26 20:43:43 +0200

    test

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log(1+2);
+function test(counter) {
+    counter--;
+    console.log("part 1 - " + counter);
+
+    if (counter != 0) {
+        test(counter);
+    }
+
+    console.log("part 2 - " + counter);
+}
